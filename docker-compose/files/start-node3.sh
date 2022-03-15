f271b81d10753c3be731cb2872ae5d65fc4bd707
Chester Ong <chester.ong.ch@gmail.com>
2022-03-15 18:25:37 +0800

    fix for avalanchego binary change and image PWD change

--- docker-compose/files/start-node3.sh
+++ docker-compose/files/start-node3.sh
@@ -7,4 +7,4 @@ sleep 10
 node1=$(getent hosts node-1 | awk '{ print $1 }')
 node2=$(getent hosts node-2 | awk '{ print $1 }')
 node3=$(getent hosts node-3 | awk '{ print $1 }')
-build/avalanchego --http-host=${node3} --public-ip=${node3} --network-id=local --db-enabled=false --staking-enabled=true --http-port=9650 --staking-port=9651 --log-level=debug --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --bootstrap-ips=${node1}:9651 --staking-tls-cert-file=/files/certs/keys3/staker.crt --staking-tls-key-file=/files/certs/keys3/staker.key
+/avalanchego/build/avalanchego --http-host=${node3} --public-ip=${node3} --network-id=local --staking-enabled=true --http-port=9650 --staking-port=9651 --log-level=debug --bootstrap-ids=NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg --bootstrap-ips=${node1}:9651 --staking-tls-cert-file=/files/certs/keys3/staker.crt --staking-tls-key-file=/files/certs/keys3/staker.key
